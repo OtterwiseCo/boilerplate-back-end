742f88a368661da3c3d43d5bf31978bb2c0300a3
João Pedro Bretanha <joaopedro@nave.rs>
2022-02-07 18:33:56 -0300

    adding auth middleware and example

--- src/routes/routes.js
+++ src/routes/routes.js
@@ -1,8 +1,26 @@
 import userRoutes from "./users-routes.js";
+import authRoutes from "./auth-routes.js";
 
-export const renderRoutes = [...Object.values(userRoutes)];
+export const renderRoutes = [
+  {
+    method: "GET",
+    url: "/health",
+    handler: (_, res) => {
+      res.status(200).send();
+    },
+  },
+  ...Object.values(userRoutes),
+  ...Object.values(authRoutes),
+];
 
 export default (fastify, opts, next) => {
+  fastify.decorateRequest("user", null);
+
+  fastify.addHook("onRequest", (req, res, next) => {
+    console.log("onRequest");
+    req.user = null;
+    next();
+  });
   for (let route of renderRoutes) {
     fastify.route(route);
   }
--- src/routes/users-routes.js
+++ src/routes/users-routes.js
@@ -1,16 +1,11 @@
+import { validateRequest } from "../middleware/auth.js";
 import * as UserController from "../controllers/user-controller.js";
 
 export default {
-  healthCheck: {
-    method: "GET",
-    url: "/health",
-    handler: (_, res) => {
-      res.status(200).send();
-    },
-  },
   getAllUsers: {
     method: "GET",
     url: "/users",
+    preHandler: [validateRequest],
     handler: UserController.index,
   },
 };
